988f37f66838c9f157bf5853fa1f563efdca2a14
LushoGV <luciano20746@gmail.com>
2022-08-02 23:10:55 -0300

    fixAll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   const changeTheme = useDarkMode();
 
   return (
-    <nav className="p-5 w-full bg-white dark:bg-slate-700 text-black dark:text-white shadow-lg flex justify-center sticky top-0">
+    <nav className="z-10 p-5 w-full bg-white dark:bg-slate-700 text-black dark:text-white shadow-lg flex justify-center sticky top-0">
       <div className="max-w-screen-xl w-full flex justify-between items-center">
         <h1 className="font-semibold text-base md:text-xl lg:px-5">Where in the world?</h1>
         <button className="font-medium text-sm md:pr-8" onClick={changeTheme}>
